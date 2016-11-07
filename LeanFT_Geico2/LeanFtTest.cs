85ee0dfe85c6781183e48dfb679cff24b2b4e155
Adam THomas <adamdthomas@gmail.com>
2016-11-07 16:35:30 -0500

    Initial Commit

---
+++ LeanFT_Geico2/LeanFtTest.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using NUnit.Framework;
+using HP.LFT.SDK;
+using HP.LFT.Verifications;
+using HP.LFT.SDK.Web;
+using HP.LFT.Report;
+using LeanFT_Geico2.Pages;
+
+namespace LeanFT_Geico2
+{
+    [TestFixture]
+    public class LeanFtTest : UnitTestClassBase
+    {
+
+
+        IBrowser browser;
+        //BankOR bankOR;
+        LoginPage loginPage;
+        static string[] names = new string[] { "bob", "frank", "sue" };
+
+        [TestFixtureSetUp]
+        public void TestFixtureSetUp()
+        {
+            // Setup once per fixture
+           
+        }
+
+        [SetUp]
+        public void SetUp()
+        {
+            // Before each test
+            browser = BrowserFactory.Launch(BrowserType.Firefox);
+            browser.ClearCache();
+            browser.Navigate(@"http://abcbank.orasi.com");
+
+            loginPage = new LoginPage(browser);
+
+            //bankOR = new BankOR(browser);
+        }
+
+        [Test]
+        [TestCaseSource(nameof(names))]
+        public void ValidLogin(string userName)
+        {
+
+            
+            //bankOR.AbcBankDemoPage.UserUsernameEditField.SetValue(userName);
+            //bankOR.AbcBankDemoPage.UserPasswordEditField.SetValue("Anypassword");
+            //bankOR.AbcBankDemoPage.LoginButton.Click();
+
+            bool loginSuccess = loginPage.ValidLogin(userName, "pass1");
+          
+
+            if (loginSuccess)
+            {
+                Reporter.ReportEvent("Login", "Tests valid login with username: " + userName, Status.Passed);
+                Assert.IsTrue(true);
+            }
+            else
+            {
+                Reporter.ReportEvent("Login", "Tests valid login with username: " + userName, Status.Failed);
+                Assert.IsTrue(false);
+            }
+
+
+            //IEditField GoogleSearchBar = browser.Describe<IEditField>(new EditFieldDescription
+            //  {
+            //      Type = @"text",
+            //      TagName = @"INPUT",
+            //      Name = @"q"
+            //  });
+
+
+            //  GoogleSearchBar.SetValue("Geico");
+        }
+
+        //[Test]
+        //public void NegativeLogin()
+        //{
+
+        //    string userName = "Adam";
+        //    //bankOR.AbcBankDemoPage.UserUsernameEditField.SetValue(userName);
+        //    //bankOR.AbcBankDemoPage.UserPasswordEditField.SetValue("Anypassword");
+        //    //bankOR.AbcBankDemoPage.LoginButton.Click();
+        //    bool loginSuccess = loginPage.ValidLogin(userName, "INValidPassword");
+
+        //    if (loginSuccess)
+        //    {
+        //        Reporter.ReportEvent("Login", "Tests valid login with username: " + userName, Status.Failed);
+        //        Assert.IsTrue(false);
+        //    }
+        //    else
+        //    {
+        //        Reporter.ReportEvent("Login", "Tests valid login with username: " + userName, Status.Passed);
+        //        Assert.IsTrue(true);
+        //    }
+        //}
+
+        //[Test]
+        //public void CheckMainTable()
+        //{
+        //    bool loginSuccess = loginPage.ValidLogin();
+        //    //Check main table
+        //}
+        [TearDown]
+        public void TearDown()
+        {
+            // Clean up after each test
+            browser.CloseAllTabs();
+        }
+
+        [TestFixtureTearDown]
+        public void TestFixtureTearDown()
+        {
+            // Clean up once per fixture
+        }
+    }
+}
---
+++ LeanFT_Geico2/Pages/LoginPage.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HP.LFT.SDK.Web;
+
+namespace LeanFT_Geico2.Pages
+{
+    public class LoginPage
+    {
+        IBrowser browser;
+        BankOR bankOR;
+        public LoginPage(IBrowser browser)
+        {
+            this.browser = browser;
+            bankOR = new BankOR(this.browser);
+        }
+        public bool ValidLogin(string userName, string password)
+        {
+            bankOR.AbcBankDemoPage.UserUsernameEditField.SetValue(userName);
+            bankOR.AbcBankDemoPage.UserPasswordEditField.SetValue(password);
+            bankOR.AbcBankDemoPage.LoginButton.Click();
+            return bankOR.AbcBankDemoPage.LogoutButton.Exists(5);
+        }
+        public bool ValidLogin()
+        {
+            bankOR.AbcBankDemoPage.UserUsernameEditField.SetValue("Adam");
+            bankOR.AbcBankDemoPage.UserPasswordEditField.SetValue("ValidPassword");
+            bankOR.AbcBankDemoPage.LoginButton.Click();
+            return bankOR.AbcBankDemoPage.LogoutButton.Exists(5);
+        }
+    }
+}
